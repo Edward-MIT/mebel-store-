fd4b284bf68b558141335ca16d17e3604c86ad3d
Edward_MIT <usmanoff19980404@gmail.com>
2025-08-28 21:12:11 +0900

    feat: integrate environtal varialbes

--- apps/batch-server/src/batch-server.module.ts
+++ apps/batch-server/src/batch-server.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BatchServerController } from './batch-server.controller';
 import { BatchServerService } from './batch-server.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [BatchServerController],
   providers: [BatchServerService],
 })
--- apps/batch-server/src/main.ts
+++ apps/batch-server/src/main.ts
@@ -3,6 +3,6 @@ import { BatchServerModule } from './batch-server.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(BatchServerModule);
-  await app.listen(process.env.port ?? 3001);
+  await app.listen(process.env.PORT_BATCH ?? 3004);
 }
 bootstrap();
