1e9c8a109e38e1901258833c9f03b577dce87ca2
Edward_MIT <usmanoff19980404@gmail.com>
2025-09-01 19:57:41 +0900

    feat: create components related module

--- apps/mebel-store/src/components/components.module.ts
+++ apps/mebel-store/src/components/components.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { PropertyModule } from './property/property.module';
+import { AuthModule } from './auth/auth.module';
+import { CommentsModule } from './comments/comments.module';
+import { LikeModule } from './like/like.module';
 
 @Module({
-  imports: [UsersModule, PropertyModule]
+  imports: [UsersModule, PropertyModule, AuthModule, CommentsModule, LikeModule]
 })
 export class ComponentsModule {}
