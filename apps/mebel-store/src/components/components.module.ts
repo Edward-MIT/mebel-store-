4ac6bb9c4b98c9d8ee49b416e09eb6707ec450e8
Edward_MIT <usmanoff19980404@gmail.com>
2025-09-09 22:41:17 +0900

    feat: viewModule and FollowModule created

--- apps/mebel-store/src/components/components.module.ts
+++ apps/mebel-store/src/components/components.module.ts
@@ -4,9 +4,20 @@ import { PropertyModule } from './property/property.module';
 import { AuthModule } from './auth/auth.module';
 import { CommentsModule } from './comments/comments.module';
 import { LikeModule } from './like/like.module';
+import { BoardArticleModule } from './board-article/board-article.module';
+import { ViewModule } from './view/view.module';
+import { FollowModule } from './follow/follow.module';
 
 
 @Module({
-  imports: [UsersModule, PropertyModule, AuthModule, CommentsModule, LikeModule],
+  imports: [
+    UsersModule,
+    PropertyModule,
+    AuthModule,
+    CommentsModule,
+    LikeModule,
+    BoardArticleModule,
+    ViewModule,
+    FollowModule],
 })
 export class ComponentsModule {}
